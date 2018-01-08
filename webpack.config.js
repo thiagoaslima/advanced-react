c0524e4be100b202e47a4ee7500984fc194d5d0e
Thiago Lima <thiagoaslima@gmail.com>
2018-01-08 18:28:34 -0200

    webpack included

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const config = {
+  entry: './lib/components/Index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+          test: /\.(js|jsx)$/, 
+          exclude: /node_modules/,
+          use: 'babel-loader' }
+    ]
+  }
+};
+
+module.exports = config;
